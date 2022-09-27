501fe1de0733fe6039a1080685290d022a892db0
Sharma_ Parve (parvesharma@deloitte.com) <parvesharma@your.hostname.com>
2022-09-27 16:26:59 +0530

    Added useEffect and useState hooks

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
@@ -11,14 +12,23 @@ const Home = () => {
       id: 3,
     },
   ]);
+  const [name, setName] = useState("mario");
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blogs) => blogs.id !== id);
+    setBlogs(newBlogs);
+  };
+  useEffect(() => {
+    console.log(name);
+  }, [name]);
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written By {blog.author}</p>
-        </div>
-      ))}
+      <BlogList
+        blogs={blogs}
+        title="All Blogs!!"
+        handleDelete={handleDelete}
+      ></BlogList>
+      <button onClick={() => setName("luigi")}>Change Name</button>
+      <p>{name}</p>
     </div>
   );
 };
